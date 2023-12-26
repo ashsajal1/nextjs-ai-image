647038f84d44f063c5581abe84f135c824fadc7e
ashsajal1 <ashsajal1@gmail.com>
2023-12-27 00:13:48 +0800

    Change model

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -4,14 +4,14 @@ export const POST = async (req: NextRequest) => {
   const { prompt } = await req.json();
 
   const response = await fetch(
-    "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1",
+    "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
     {
       headers: { Authorization: process.env.HUGGING_FACE_KEY as string },
       method: "POST",
       body: JSON.stringify({ inputs: prompt }),
     }
   );
-  
+
   const blob = await response.blob();
   const arrayBuffer = await blob.arrayBuffer();
   const base64 = Buffer.from(arrayBuffer).toString("base64");

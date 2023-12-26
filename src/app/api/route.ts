60386c2034b9b4b0fe3c9a0748b4f319a1dfc193
ashsajal1 <ashsajal1@gmail.com>
2023-12-26 22:43:58 +0800

    Update route and  page

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,19 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
-  const {prompt} = await req.json();
+  const { prompt } = await req.json();
 
   const response = await fetch(
     "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1",
     {
       headers: { Authorization: process.env.HUGGING_FACE_KEY as string },
       method: "POST",
-      body: JSON.stringify({"inputs": prompt}),
+      body: JSON.stringify({ inputs: prompt }),
     }
   );
-  const result = await response.blob();
- 
-  return NextResponse.json({
-    result
-  })
+  
+  const blob = await response.blob();
+  const arrayBuffer = await blob.arrayBuffer();
+  const base64 = Buffer.from(arrayBuffer).toString("base64");
+  return NextResponse.json({ base64 });
 };
